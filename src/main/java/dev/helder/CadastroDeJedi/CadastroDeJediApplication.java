da6e1fb3fc3d61bdad5124c8a8490d27f696e463
Hélder Ferreira <helder654@gmail.com>
2026-07-16 18:45:06 -0300

    first commit

---
+++ src/main/java/dev/helder/CadastroDeJedi/CadastroDeJediApplication.java
@@ -0,0 +1,13 @@
+package dev.helder.CadastroDeJedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeJediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeJediApplication.class, args);
+	}
+
+}
