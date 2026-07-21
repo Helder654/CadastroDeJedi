34f01dd6da72407f9e967e96b9b32328a36de666
Hélder Ferreira <helder654@gmail.com>
2026-07-21 20:59:51 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/helder/CadastroDeJedi/Jedi/Controller/JediController.java
+++ src/main/java/dev/helder/CadastroDeJedi/Jedi/Controller/JediController.java
@@ -1,7 +1,10 @@
 package dev.helder.CadastroDeJedi.Jedi.Controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,35 @@ public class JediController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota";
+    }
+    
+    //Mock de endpoints
+    //Adicionar jedi(CREATE)
+    @PostMapping("/criar")
+    public String criarJedi(){
+        return "ninja criado";
+    }
+
+    //Mostrar todos os jedi(READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsJedi(){
+        return "mostrar jedi";
+    }
+
+    //Mostrar jedi por id(READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsJediPorId(){
+        return "mostrar jedi por id";
+    }
+    //Alterar dados do jedi(UPDATE)
+    @PutMapping("alterarID")
+    public String alterarJediPorId(){
+        return"jedi alterado";
+    }
+
+    //Deletar jedi(DELETE)
+    @DeleteMapping("/deleteID")
+    public String deletarJediPorId(){
+        return"jedi deletado por id";
     }
 }
