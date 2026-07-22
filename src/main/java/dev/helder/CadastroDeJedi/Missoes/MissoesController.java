4c0a7e8690f5c98ccdcad0b28288cae2e7f6e5ee
Hélder Ferreira <helder654@gmail.com>
2026-07-22 00:44:27 -0300

    feat: cria as rotas do controller com mock

--- src/main/java/dev/helder/CadastroDeJedi/Missoes/MissoesController.java
+++ src/main/java/dev/helder/CadastroDeJedi/Missoes/MissoesController.java
@@ -1,5 +1,28 @@
 package dev.helder.CadastroDeJedi.Missoes;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//LOCALHOST:8080/DELETAR
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
 
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "missão criada com sucesso";
+    }
+    
+    @PutMapping("/alterar")
+    public String editarMissao(){
+        return "missão editada com sucesso";
+    }
+
+    @DeleteMapping("/delete")
+    public String deletarMissao(){
+        return"missao deletada";
+    }
 }
