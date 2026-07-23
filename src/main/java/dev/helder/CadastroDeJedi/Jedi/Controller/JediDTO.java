972c8ee8e59737c3a16c0b130c2bb42b17c0bd59
Hélder Ferreira <helder654@gmail.com>
2026-07-23 02:58:55 -0300

    chore: Inclui a versao da DTO pela entidade e mapeia manualmente no JediMapperos caminho de entidade x dto

--- src/main/java/dev/helder/CadastroDeJedi/Jedi/Controller/JediDTO.java
+++ src/main/java/dev/helder/CadastroDeJedi/Jedi/Controller/JediDTO.java
@@ -1,5 +1,21 @@
 package dev.helder.CadastroDeJedi.Jedi.Controller;
 
+import dev.helder.CadastroDeJedi.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class JediDTO {
 
+    private Long id;
+    private String nome;
+    private int idade;
+    private String email;
+    private MissoesModel missoes;
+    private String rank;
 }
--- src/main/java/dev/helder/CadastroDeJedi/Jedi/Controller/JediMapper.java
+++ src/main/java/dev/helder/CadastroDeJedi/Jedi/Controller/JediMapper.java
@@ -1,5 +1,39 @@
 package dev.helder.CadastroDeJedi.Jedi.Controller;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JediMapper {
 
+    public JediModel map(JediDTO jediDTO){
+        JediModel jediModel = new JediModel();
+        
+        jediModel.setId(jediDTO.getId());
+        jediModel.setNome(jediDTO.getNome());
+        jediModel.setEmail(jediDTO.getEmail());
+        jediModel.setIdade(jediDTO.getIdade());
+        jediModel.setMissoes(jediDTO.getMissoes());
+        jediModel.setRank(jediDTO.getRank());
+
+        return jediModel;
+    }
+
+    public JediDTO map(JediModel jediModel){
+        JediDTO jediDTO = new JediDTO();
+        
+        jediDTO.setEmail(jediModel.getEmail());
+        jediDTO.setId(jediModel.getId());
+        jediDTO.setIdade(jediModel.getIdade());
+        jediDTO.setMissoes(jediModel.getMissoes());
+        jediDTO.setNome(jediModel.getNome());
+        jediDTO.setRank(jediModel.getRank());
+
+        return jediDTO;
+
+
+
+
+
+
+    }
 }
