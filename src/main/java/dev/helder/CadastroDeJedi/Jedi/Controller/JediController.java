7ce8bd63c275c976e6dfb07ce57e2f6391d609e6
Hélder Ferreira <helder654@gmail.com>
2026-07-27 01:26:22 -0300

    refactor: Refatora o JediController para incluir o responseEntity nas minhas rotas e customizar as respostas da requisição dos clientes

--- src/main/java/dev/helder/CadastroDeJedi/Jedi/Controller/JediController.java
+++ src/main/java/dev/helder/CadastroDeJedi/Jedi/Controller/JediController.java
@@ -2,6 +2,8 @@ package dev.helder.CadastroDeJedi.Jedi.Controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,31 +31,56 @@ public class JediController {
     //Mock de endpoints
     //Adicionar jedi(CREATE)
     @PostMapping("/criar")
-    public JediDTO criarJedi(@RequestBody JediDTO jedi){
-        return jediService.criarJedi(jedi);
+    public ResponseEntity<String> criarJedi(@RequestBody JediDTO jedi){
+        JediDTO novoJedi = jediService.criarJedi(jedi);
+        return ResponseEntity.status(HttpStatus.CREATED)
+        .body("Jedi criado com sucesso: " + novoJedi.getNome() + " (ID): " + novoJedi.getId());
     }
 
     //Mostrar todos os jedi(READ)
     @GetMapping("/listar")
-    public List<JediDTO> mostrarTodosOsJedi(){
-        return jediService.listarJedi();
+    public ResponseEntity<List<JediDTO>> mostrarTodosOsJedi(){
+        List<JediDTO> jediLista = jediService.listarJedi();   
+        return ResponseEntity.ok(jediLista);
     }
 
     //Mostrar jedi por id(READ)
     @GetMapping("/listar/{id}")
-    public JediDTO mostrarOsJediPorId(@PathVariable Long id){
-        return jediService.listarJediPorId(id);
+    public ResponseEntity<?> mostrarOsJediPorId(@PathVariable Long id){
+
+        JediDTO jedi = jediService.listarJediPorId(id);
+
+        if(jedi != null){
+            return ResponseEntity.ok(jedi);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body("O Jedi com Id " + id + " Não encontrado");
+        }
     }
 
     //Alterar dados do jedi(UPDATE)
     @PutMapping("/alterar/{id}")
-    public JediDTO alterarJediPorId(@PathVariable Long id, @RequestBody JediDTO jediAtualizado){
-        return jediService.atualizarJedi(id, jediAtualizado);
+    public ResponseEntity<?> alterarJediPorId(@PathVariable Long id, @RequestBody JediDTO jediAtualizado){
+        
+        JediDTO jedi = jediService.atualizarJedi(id, jediAtualizado);
+
+        if(jedi != null){
+            return ResponseEntity.ok(jedi);
+        } else {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body("Id de Jedi não encontrado");
+        }
     }
 
     //Deletar jedi(DELETE)
     @DeleteMapping("/delete/{id}")
-    public void deletarJediPorId(@PathVariable Long id){
-        jediService.deletarJediPorId(id);
+    public ResponseEntity<String> deletarJedi(@PathVariable Long id){
+        if(jediService.listarJediPorId(id) != null){
+            jediService.deletarJediPorId(id);
+            return ResponseEntity.ok("Jedi do Id: " + id + " deletado com sucesso");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("O Jedi com Id " + id + " Não encontrado");
+        }
     }
 }
